ca0a047eca90aae9b0b093ff05487dd86e21b38e
saiffardin <saiffardin@gmail.com>
2020-08-03 21:27:37 +0600

    assignment 4 initially done

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var currNum = ''; // Current number
-var GeneratedPin = '8888';
+var GeneratedPin = '';
 var tryNumbers = 3;
+var pinNotGenerated = true;
 
 const numPad = getElement('.num');
 const screen = getElement('#screen');
 const trySpan = getElement('#try-left');
 const wrongPassword = getElement('#wrong');
 const correctPassword = getElement('#correct');
+const genPinScreen = getElement('#generate-pin-screen');
+const allInputBtns = getElement('.numbers');
+
 
 // --------------------------------------------------
 
@@ -18,6 +22,8 @@ function getElement(element) {
 
     return document.querySelectorAll(element);
 }
+
+
 
 // When Number is clicked. Get the current number selected
 var setNum = function () {
@@ -35,7 +41,43 @@ var setNum = function () {
 };
 
 
+// if pin not generated
+var notGenerated = function (e) {
+    if (pinNotGenerated) {
+        alert("Generate your 4 digit pin first.");
+        // console.log("Generate your 4 digit pin first.");
+        // return;
+        e.stopPropagation();
+    }
+};
+
 // --------------------------------------------------
+
+
+// clicked generate btn
+getElement('#generate-btn').addEventListener('click', function () {
+
+    wrongPassword.style.display = 'none';
+    correctPassword.style.display = 'none';
+
+
+    pinNotGenerated = false;
+
+    var rn = Math.floor(Math.random() * (9999 - 1000)) + 1000;
+    console.log(rn);
+
+    genPinScreen.value = rn;
+    GeneratedPin = String(rn);
+});
+
+
+// clicked any btn in number pad
+for (var btn of allInputBtns) {
+    btn.addEventListener('click', notGenerated, true);
+
+}
+
+
 
 // clicked number pad
 for (var num of numPad) {
@@ -55,6 +97,7 @@ getElement('#clear').addEventListener('click', function () {
 
 // clicked submit btn
 getElement('#submit-btn').addEventListener('click', function () {
+
     var enteredPin = screen.value;
 
     if (enteredPin.length == 0) {
@@ -62,28 +105,36 @@ getElement('#submit-btn').addEventListener('click', function () {
     }
 
     else {
-        console.log(tryNumbers);
+        // console.log(tryNumbers);
 
         if (GeneratedPin === enteredPin) {
             correctPassword.style.display = 'block';
+            // genPinScreen.value = "";
+
+            // tryNumbers = 3;
+            // trySpan.innerHTML = tryNumbers;
         }
 
-        else if (tryNumbers>0) {
+        else if (tryNumbers > 0) {
             wrongPassword.style.display = 'block';
+            tryNumbers--;
+            trySpan.innerHTML = tryNumbers;
         }
 
-        tryNumbers--;
-        trySpan.innerHTML = tryNumbers;
-        
 
-        if (tryNumbers==0){
+
+
+        if (tryNumbers == 0) {
             wrongPassword.style.display = 'none';
             getElement('#submit-btn').disabled = true;
             getElement('#submit-btn').style.background = 'lightgray';
-     
+
         }
+
+        screen.value = "";
     }
 
-    screen.value = "";
+
 
 });
+
